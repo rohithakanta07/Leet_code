c5ac2c59851f2fa04658cb377831beb8d99f58a0
rohithakanta07 <119839280+rohithakanta07@users.noreply.github.com>
2024-02-06 21:43:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (24.57%) - LeetSync

---
+++ 2752-sum-multiples/sum-multiples.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int s = 0 ;
+        for(int i = 1 ; i<= n ; i++)
+        {
+            if(i%3 == 0 or i % 5 == 0 or i % 7 == 0)
+            {
+                s = s+i;
+            }
+        }
+        return s;
+    }
+};
