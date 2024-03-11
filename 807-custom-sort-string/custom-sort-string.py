931300c9d168922a8595770d01137f42ef30838a
rohithakanta07 <119839280+rohithakanta07@users.noreply.github.com>
2024-03-11 13:06:47 +0530

    Time: 39 ms (28.74%) | Memory: 16.6 MB (26.18%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -2,24 +2,21 @@ class Solution:
     def customSortString(self, order: str, s: str) -> str:
         if(order == ""):
             return s 
-        news=""
+        ns=""
         d={}
         for i in s :
             if i not in d :
                 d[i]=1
             else:
                 d[i]+=1
-        for i in order :
-            if i in s :
+        for i in order:
+            if i in s:
                 while(d[i]!=0):
-                    news=news+i
+                    ns+=i
                     d[i]-=1
-        # for i in d:
-        #     if d[i]!=0:
-        #         news+=i
-        #         d[i]-=1
         for i in d:
-            while(d[i]!=0):
-                news+=i
-                d[i]-=1
-        return news
+            if d[i]!=0:
+                while(d[i]!=0):
+                    ns+=i
+                    d[i]-=1
+        return ns
