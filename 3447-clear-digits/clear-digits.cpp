5cdca50b00a7253210b048ecb0ac109d10a6ad65
RohithaKantac <119839280+rohithakanta07@users.noreply.github.com>
2024-09-05 22:39:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (27.11%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char>st;
+        for(int i = 0 ; i<s.size();i++){
+            if(isdigit(s[i])){
+                if(!st.empty()){
+                    st.pop();
+                }
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        string s1="";
+        while(!st.empty()){
+            s1+=st.top();
+            st.pop();
+        }
+        reverse(s1.begin(),s1.end());
+        return s1;
+    }
+};
