33532a62c68a5136f93e6f0412fb6581a2e327b5
RohithaKantac <119839280+rohithakanta07@users.noreply.github.com>
2025-03-21 17:56:21 +0530

    Time: 415 ms (5.64%) | Memory: 115.8 MB (16.92%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int countDistinctIntegers(vector<int>& nums) {
-        map<int,int>mp;
+        set<int>st;
         for(int i = 0 ; i<nums.size() ; i++){
-            if(mp.find(nums[i]) ==mp.end()) mp[nums[i]]++;
+            st.insert(nums[i]);
             string s = to_string(nums[i]);
             reverse(s.begin(),s.end());
             int a = stoi(s);
             // cout<<a<<" ";
-            if(mp.find(a) ==mp.end()) mp[a]++;
+            st.insert(a);
         }
-        return mp.size();
+        return st.size();
     }
 };
