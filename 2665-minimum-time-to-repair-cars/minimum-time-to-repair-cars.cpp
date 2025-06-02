599aafdd5a76d78c3caa029ff9b51d24ca93ece6
RohithaKantac <119839280+rohithakanta07@users.noreply.github.com>
2025-06-02 21:50:51 +0530

    Time: 53 ms (47.85%) | Memory: 59.4 MB (49.28%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+        long long l = 0 , mi = INT_MAX;
+        long long m = *max_element(ranks.begin() , ranks.end());
+        long long h = m * cars*cars;
+        // sort(ranks.begin(),ranks.end());
+        while(l<h){
+            long long mid = (l+h)/2;
+            // vector<int>c(ranks.size());
+            long long sum =0 ;
+            for(int i = 0 ; i < ranks.size();i++){
+                sum+=(long long)sqrt((mid/ranks[i]));
+            }
+            // if(sum == cars){
+            //     mi = min(mid,mi);
+            //     break;
+            // }
+            if (sum >= cars ){
+                h = mid;
+            }
+            else {
+                l = mid+1;
+            }
+            // cout<<mid<<" "<<sum<<" "<<endl;
+        }
+        return l;
+    }
+};
